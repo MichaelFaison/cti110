d3d5f79c71ff70c46ae1ee9c266cd9530fd74527
Michael Faison <47933718+MichaelFaison@users.noreply.github.com>
2019-06-20 15:19:28 -0400

    Add files via upload

---
+++ P2HW1_PoundsKilograms_MichaelFaison.py
@@ -0,0 +1,19 @@
+#This program converts pounds to kilograms
+
+#06/20/2019
+
+#CTI-110 P2HW1- Pounds to Kilograms Converter
+
+#Michael Faison
+
+
+
+
+#Title of the program
+print("Pounds to Kilograms.")
+#prompting the user to enter how many pounds they want to convert
+lbs = int(input("How many pounds do you want to convert? "))
+#This is the calculation 
+kilograms = lbs/2.2046
+#This gives the result of the calculations
+print("=", kilograms, "in kilograms")
